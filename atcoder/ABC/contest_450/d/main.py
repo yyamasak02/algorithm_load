62ac5a77b9442b612499c71ab3802424c28ebe4f
yuhe-02 <domineter4649@gmail.com>
2026-04-09 23:36:00 +0900

    d問題解けた

--- atcoder/ABC/contest_450/d/main.py
+++ atcoder/ABC/contest_450/d/main.py
@@ -1,4 +1,14 @@
 def main():
+    N, K = map(int, input().split())
+    array = list(map(int, input().split()))
+    for i in range(N):
+        array[i] = array[i] % K
+    array.sort()
+    min_diff = 10**9
+    for i in range(N - 1):
+        min_diff = min(min_diff, array[i] + K - array[i + 1])
+    min_diff = min(min_diff, abs(array[-1] - array[0]))
+    print(min_diff)
     return
 
 

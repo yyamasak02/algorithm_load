290a3ef222d89c0337895568849404124c3be948
yuhe-02 <domineter4649@gmail.com>
2026-06-02 23:48:20 +0900

    Add solutions for AtCoder ABC contest 457

--- atcoder/ABC/contest_457/c/main.py
+++ atcoder/ABC/contest_457/c/main.py
@@ -1,4 +1,25 @@
 def main():
+    N, K = map(int, input().split())
+    L = []
+    a_list = []
+    for _ in range(N):
+        tmp = list(map(int, input().split()))
+        L.append(tmp[0])
+        a_list.append(tmp[1::])
+    C = list(map(int, input().split()))
+    index, counter = 0, 0
+    while counter < K:
+        Li = L[index]
+        Ai = a_list[index]
+        Ci = C[index]
+        if (counter + Ci * Li) < K:
+            counter += Ci * Li
+            index += 1
+            continue
+        target_index = K - counter
+        # print(Li, Ai, Ci)
+        print(Ai[(target_index % Li) - 1])
+        break
     return
 
 
--- atcoder/ABC/contest_457/d/main.py
+++ atcoder/ABC/contest_457/d/main.py
@@ -1,4 +1,26 @@
 def main():
+    N, K = map(int, input().split())
+    A = list(map(int, input().split()))
+    A.insert(0, 0)
+    left, right = 1, A[1] + K + 1
+
+    def isok(x):
+        # ans を x 以上にすることができますか？
+        nk = 0
+        for i in range(1, N + 1):
+            if A[i] < x:
+                nk += (x - A[i] + i - 1) // i
+                if nk > K:
+                    return False
+        return True
+
+    while right - left > 1:
+        mid = (right + left) // 2
+        if isok(mid):
+            left = mid
+        else:
+            right = mid
+    print(left)
     return
 
 
